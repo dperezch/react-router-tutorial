f183ef5f9710ce3fd4d794b5b4eba7d28a0653dd
dperezch <davarmando@gmail.com>
2023-03-10 20:04:18 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Inicio from "./pages/Inicio";
+import Contacto from "./pages/Contacto";
+import Blog from "./pages/Blog";
+import LayoutPublic from "./layouts/LayoutPublic";
+import NotFound from "./pages/NotFound";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<LayoutPublic />}>
+            <Route element={<Inicio />} path="/"></Route>
+            <Route element={<Contacto />} path="/contacto" />
+            <Route element={<Blog />} path="/blog" />
+            
+          </Route>
+          <Route element={<NotFound/>} path="*" ></Route>
+        </Routes>
+      </div>
+    </>
+  );
+};
+
+export default App;
